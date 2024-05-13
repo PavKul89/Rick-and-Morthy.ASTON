c3556863bac69f91f0b165697bed0c1ddb0f8a5b
Pavel <PavKul89@gmail.com>
2024-05-13 11:46:53 +0300

    feat:search result

--- src/components/Header/History.jsx
+++ src/components/Header/History.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
 
 function History() {
-  // Получаем параметры URL
-  const searchParams = new URLSearchParams(window.location.search)
-  const query = searchParams.get('query')
-
   return (
     <div>
-      <h1>History</h1>
-      <p>{query}</p>
-      {/* Здесь можно использовать результаты поиска из параметров URL */}
+      <div>History</div>
     </div>
   )
 }
